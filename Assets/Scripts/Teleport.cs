474110b2d62e7e54f9e3d904931ecd77c2374777
leonardo <leonardodimarchi@outlook.com>
2021-12-29 08:24:11 -0300

    init(*): commiting the project

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public void Move(Transform obj) 
+    {
+        transform.position = new Vector3(obj.position.x, transform.position.y, obj.position.z);
+    }
+}
